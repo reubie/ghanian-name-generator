1851e6b8839bd52770034f7416d7af4468e7ff44
reubie <r.nguyo27@gmail.com>
2021-09-03 10:27:26 +0300

    create variable for male and female names plus days of the week

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// creation of variables
+
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akuso","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
