a593a4dbbde551f7b7926122eee951e46a9ba4d7
reubie <r.nguyo27@gmail.com>
2021-09-04 13:48:38 +0300

    add footer and create sections for logic in js file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-// Business (or back-end) logic:
+// User interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
+      event.preventDefault();
+      var y = parseInt($("#year").val());
+      var m = parseInt($("#month").val());
+      var d = parseInt($("#date").val());
+      var g = $("input:radio[name=gender]:checked").val();
+      var result = akan(y, m, d, g);
+      alert("Your akan name is: " + result);
+
+
+      // Business (or back-end) logic:
+      
 var akan = function(y, m, d, g) {
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
@@ -10,16 +24,6 @@ var akan = function(y, m, d, g) {
   }
 }
 
-// User interface (or front-end) logic:
-$(document).ready(function() {
-  $("form#form").submit(function(event) {
-      event.preventDefault();
-      var y = parseInt($("#year").val());
-      var m = parseInt($("#month").val());
-      var d = parseInt($("#date").val());
-      var g = $("input:radio[name=gender]:checked").val();
-      var result = akan(y, m, d, g);
-      alert("Your akan name is: " + result);
       //refresh page
       document.getElementById("form").reset();
   });
