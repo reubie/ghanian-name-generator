5407806f3a14891b5c37815475a0b8cc15b7d5a8
reubie <r.nguyo27@gmail.com>
2021-09-04 09:37:22 +0300

    modified the input data form

--- js/script.js
+++ js/script.js
@@ -36,6 +36,19 @@ function check() {
     
   }
 
+  // create a function to calculate the day of the week
+
+  function calculatepositionDay(){
+    year = document.getElementById("year").value;
+    CC = parseInt(year.substring(0,2));
+    YY = parseInt(year.substring(2,4));
+    MM = parseInt(document.getElementById("month").value);
+    DD = parseInt(document.getElementById("date").value);
+    d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+    console.log(d);
+    return (Math.floor(d));
+}
+
   
 // create a function to pick gender
 
@@ -103,19 +116,6 @@ function getGender(){
     default:
     }
 }
-
-// create a function to calculate the day of the week
-
-    function calculatepositionDay(){
-        year = document.getElementById("year").value;
-        CC = parseInt(year.substring(0,2));
-        YY = parseInt(year.substring(2,4));
-        MM = parseInt(document.getElementById("month").value);
-        DD = parseInt(document.getElementById("date").value);
-        d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
-        console.log(d);
-        return (Math.floor(d));
-    }
 
 // create the call fucntion for the generate button
 
