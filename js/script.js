dc2c68d0f881dd70a2c1c7dc50ac67272b733d72
reubie <r.nguyo27@gmail.com>
2021-09-04 13:58:36 +0300

    modification of the js and index file to display names on popup

--- js/script.js
+++ js/script.js
@@ -1,30 +1,26 @@
-// User interface (or front-end) logic:
-
-$(document).ready(function() {
-  $("form#form").submit(function(event) {
-      event.preventDefault();
-      var y = parseInt($("#year").val());
-      var m = parseInt($("#month").val());
-      var d = parseInt($("#date").val());
-      var g = $("input:radio[name=gender]:checked").val();
-      var result = akan(y, m, d, g);
-      alert("Your akan name is: " + result);
-
-
-      // Business (or back-end) logic:
-      
+// Business (or back-end) logic:
 var akan = function(y, m, d, g) {
-  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  var d = new Date(y, --m, d);
-  if (g === "Female") {
-      return d && femaleNames[d.getDay()];
-  } else {
-      return d && maleNames[d.getDay()];
-  }
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var d = new Date(y, --m, d);
+    if (g === "Female") {
+        return d && femaleNames[d.getDay()];
+    } else {
+        return d && maleNames[d.getDay()];
+    }
 }
 
-      //refresh page
-      document.getElementById("form").reset();
-  });
+// User interface (or front-end) logic:
+$(document).ready(function() {
+    $("form#form").submit(function(event) {
+        event.preventDefault();
+        var y = parseInt($("#year").val());
+        var m = parseInt($("#month").val());
+        var d = parseInt($("#date").val());
+        var g = $("input:radio[name=gender]:checked").val();
+        var result = akan(y, m, d, g);
+        alert("Your akan name is: " + result);
+        //refresh page
+        document.getElementById("form").reset();
+    });
 });
